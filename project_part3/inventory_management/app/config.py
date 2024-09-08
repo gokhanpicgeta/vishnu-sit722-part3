6145bd362678a6cc4366c3d3fa8caf2054419c3c
gokhanpicgeta <vishnuprasad97@hotmail.com>
2024-09-08 22:07:31 +1000

    Added docker-compose, dockerfiles and deployment files. Changed python files to not be relative imports too

--- project_part3/inventory_management/app/config.py
+++ project_part3/inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://sit722_7_2_vish_user:A9Q2dhe4cnI2F1XHopJtn31rplXS4KAY@dpg-crem91qj1k6c73de9sh0-a.oregon-postgres.render.com/sit722_7_2_vish')
